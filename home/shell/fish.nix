19877d6a2fe0f5f2bbc321af01f29e2c88913e11
neonscar <neonscar@localhost>
2026-03-03 14:56:40 -0700

    Fix: Force custom config.fish to resolve Home Manager conflict

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.fish.enable = true;
   
-  # Link individual config files instead of the whole directory
-  xdg.configFile."fish/config.fish".source = ./config/config.fish;
+  # Use mkForce to override Home Manager's auto-generated config.fish
+  xdg.configFile."fish/config.fish".source = lib.mkForce ./config/config.fish;
+  
+  # Link functions and conf.d
   xdg.configFile."fish/functions".source = ./config/functions;
   xdg.configFile."fish/conf.d".source = ./config/conf.d;
+  xdg.configFile."fish/themes".source = ./config/themes;
+  xdg.configFile."fish/completions".source = ./config/completions;
 
   home.packages = with pkgs; [ eza fastfetch ripgrep bat fzf starship ];
 }
