98c66862e153f8c8ecdb10483adb66f37fff11d0
s197Guy <s197racer@gmail.com>
2026-03-05 03:07:52 -0700

    Fix syntax error in swappy.nix (incorrect multiline string)

--- home/style/swappy.nix
+++ home/style/swappy.nix
@@ -1,5 +1,5 @@
 { config, ... }: {
-  home.file.".config/swappy/config".text = """
+  home.file.".config/swappy/config".text = ''
 [Default]
 save_dir=${config.home.homeDirectory}/Pictures/Screenshots
 save_filename_format=Screenshot-%Y%m%d-%H%M%S.png
@@ -10,5 +10,5 @@ text_font=JetBrainsMono Nerd Font
 paint_mode=brush
 early_exit=true
 fill_shape=false
-""";
+'';
 }
