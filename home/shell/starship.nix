d0eca29a6a1e18311885002e08b337425c47e536
s197Guy <s197racer@gmail.com>
2026-03-13 13:14:26 -0700

    Remove background color from Starship prompt for transparency

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -3,7 +3,7 @@
     enable = true;
     enableFishIntegration = true;
     settings = {
-      format = ''[ ](bg:#2d353b)$username$hostname$directory$git_branch$git_status$character'';
+      format = ''$username$hostname$directory$git_branch$git_status$character'';
       
       # Everforest Color Palette (Hard)
       # bg: #2d353b
@@ -14,18 +14,18 @@
 
       username = {
         show_always = true;
-        style_user = "bg:#2d353b fg:#a7c080";
+        style_user = "fg:#a7c080";
         format = "[$user]($style)";
       };
 
       hostname = {
         ssh_only = false;
-        style = "bg:#2d353b fg:#7fbbb3";
+        style = "fg:#7fbbb3";
         format = "[@$hostname]($style) ";
       };
 
       directory = {
-        style = "bg:#2d353b fg:#dbbc7f";
+        style = "fg:#dbbc7f";
         format = "[$path]($style) ";
         truncation_length = 3;
         truncation_symbol = "../";
@@ -33,12 +33,12 @@
 
       git_branch = {
         symbol = " ";
-        style = "bg:#2d353b fg:#a7c080";
+        style = "fg:#a7c080";
         format = "[on $symbol$branch]($style) ";
       };
 
       git_status = {
-        style = "bg:#2d353b fg:#e67e80";
+        style = "fg:#e67e80";
         ahead = "⇡ ";
         behind = "⇣ ";
         diverged = "⇕ ";
